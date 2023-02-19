2991451b2d0524e5e3a70d28723de468ff714b7a
Aldmsk <Aldsmsk@gmail.com>
2023-02-19 13:45:06 +0300

    Seminar is finish

---
+++ task1.py
@@ -0,0 +1,25 @@
+# Задача 1
+# n = int(input())
+# m = int(input())
+# result = (n + m - 1) // n
+# print(result)
+
+# # Задача 2
+# a = int(input()) + 1
+# b = int(input()) + 1
+# c = int(input()) + 1
+# a //= 2
+# b //= 2
+# c //= 2
+# print(a + b + c)
+
+
+
+# b = int(input())
+# x = b // 3
+# print(x + x)
+# print(x // 2)
+
+# n = int(input('Введите год: '))
+# print(f'{n % 4 == 0 and n % 100 != 0 or n % 400 == 0}')
+
